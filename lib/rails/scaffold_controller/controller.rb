a3d506ab688c1b3d4e2b03461520dedd9030a754
El Retirao <elretirao@elretirao.net>
2010-07-28 19:28:35 +0200

    before_filter para todas las acciones, asignando el elemento.

--- lib/rails/scaffold_controller/controller.rb
+++ lib/rails/scaffold_controller/controller.rb
@@ -1,6 +1,8 @@
 class <%= controller_class_name %>Controller < ApplicationController
   respond_to :html
   before_filter :find_<%= file_name %>, :only => :show, :edit, :update, :destroy
+  before_filter :new_<%= file_name %>, :only => [:new, :create]
+  before_filter :update_<%= file_name %>, :only => :update
 
 <% unless options[:singleton] -%>
   def index
@@ -14,7 +16,6 @@ class <%= controller_class_name %>Controller < ApplicationController
   end
 
   def new
-    @<%= file_name %> = <%= orm_class.build(class_name) %>
     respond_with(@<%= file_name %>)
   end
 
@@ -22,13 +23,12 @@ class <%= controller_class_name %>Controller < ApplicationController
   end
 
   def create
-    @<%= file_name %> = <%= orm_class.build(class_name, "params[:#{file_name}]") %>
     <%= "flash[:notice] = '#{class_name} was successfully created.' if " %>@<%= orm_instance.save %>
     respond_with(@<%= file_name %>)
   end
 
   def update
-    <%= "flash[:notice] = '#{class_name} was successfully updated.' if " %>@<%= orm_instance.update_attributes("params[:#{file_name}]") %>
+    <%= "flash[:notice] = '#{class_name} was successfully updated.' if " %>@<%= orm_instance.save %>
     respond_with(@<%= file_name %>)
   end
 
@@ -40,5 +40,13 @@ class <%= controller_class_name %>Controller < ApplicationController
 private
   def find_<%= file_name %>
     @<%= file_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+  end
+
+  def new_<%= file_name %>
+    @<%= file_name %> = <%= orm_class.build(class_name, "params[:#{file_name}]") %>
+  end
+
+  def update_<%= file_name %>
+    @<%= file_name %>.attributes = params[:<%= file_name %>]
   end
 end
