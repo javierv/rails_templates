658a702089420e251bc68d8098d86b61a0e97c82
El Retirao <elretirao@elretirao.net>
2010-08-01 21:10:19 +0200

    Sustituimos métodos que no funcionaban.
    No sé cómo van las cosas internas de orm_class, pero parece que no coge los métodos de módulos añadidos automáticamente.

--- lib/rails/scaffold_controller/controller.rb
+++ lib/rails/scaffold_controller/controller.rb
@@ -9,9 +9,9 @@ class <%= controller_class_name %>Controller < ApplicationController
 
 <% unless options[:singleton] -%>
   def index
-    @search = <%= orm_class.search(class_name, "params[:search]") %>
+    @search = <%= class_name %>.search params[:search]
     @<%= table_name %> = @search.paginate :page => params[:page],
-      :per_page => <%= orm_class.per_page(class_name) %>
+      :per_page => <%= class_name %>.per_page
     respond_with @<%= table_name %>
   end
 <% end -%>
