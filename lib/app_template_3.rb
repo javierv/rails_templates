1a001593ca5edc3462b502a8ae879f629554320e
El Retirao <elretirao@elretirao.net>
2010-08-01 21:53:21 +0200

    Usamos filtrador en el índice.

--- lib/app_template_3.rb
+++ lib/app_template_3.rb
@@ -7,7 +7,7 @@ gem 'acts-as-taggable-on'
 gem 'validation_reflection'
 gem 'RedCloth'
 gem 'validates_date_time'
-gem 'simple_form'
+gem 'simple_form', :git => "/home/javi/programas/work/simple_form"
 gem 'meta_where'
 gem 'meta_search'
 gem 'tabletastic', :git => "/home/javi/programas/work/tabletastic"
